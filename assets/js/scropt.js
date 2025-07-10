3ed85d289ead00ea36ec217dc32e23fa2e3f1fbc
dpalexandru <alexpoian91@hotmail.com>
2025-07-10 15:26:20 +0200

    feat: function to calcolate ticket price

--- assets/js/scropt.js
+++ assets/js/scropt.js
@@ -1,3 +1,27 @@
+//Funzione per calcolare il prezzo del biglietto 
+
+const calcolaPrezzo = (km,eta) => {
+let prezzoAlKm = 0.21; 
+let scontoMinore = 0.20;
+let scontoPensione =0.40;
+let prezzoViaggio = 0; 
+
+if (eta < 18) {
+  prezzoViaggio = km * prezzoAlKm
+  prezzoViaggio = prezzoViaggio - (prezzoViaggio * scontoMinore)
+} else if (eta >= 65) {
+  prezzoViaggio = km * prezzoAlKm
+  prezzoViaggio = prezzoViaggio - (prezzoViaggio * scontoPensione)
+}
+else{
+  prezzoViaggio = km * prezzoAlKm
+}
+return prezzoViaggio;
+} 
+
+//test in console log della funzione calcola prezzo 
+console.log(calcolaPrezzo(100,13))
+
 //Dichiaro e recupero il button
 const button = document.getElementById('button')
 
